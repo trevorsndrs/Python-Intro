471ed35f5db1501eaf34b6fc2f54a79e8e7893ae
trevorsndrs <156527181+trevorsndrs@users.noreply.github.com>
2024-01-13 09:56:36 -0500

    Create magic8ball.py
    My first real attempt at applying python using the Code Academy project.

---
+++ magic8ball.py
@@ -0,0 +1,39 @@
+import random
+#Your name here!
+name = ""
+#Your question here!
+question = ""
+#No imputs beyond this point! "You shall not pass!"
+answer = ""
+random_number = random.randint(1, 9)
+
+if random_number == 1:
+ answer = "Yes, Definitely"
+elif random_number == 2:
+  answer = "It is decidedly so"
+elif random_number == 3:
+  answer = "Without a doubt"
+elif random_number == 4:
+  answer = "Reply hazy, try again"
+elif random_number == 5:
+  answer = "Ask again later"
+elif random_number == 6:
+  answer = "Better not tell you now"
+elif random_number == 7:
+  answer = "My sources say no"
+elif random_number == 8:
+  answer = "Outlook not so good"
+elif random_number == 9:
+  answer = "Very doubtful"
+else:
+  print("Error")
+
+if name == "":
+ print("Question: " + question)
+else:
+  print(name + " asks: " + question)
+
+if question == "":
+  print("You didn't ask me a question!")
+else:
+  print("Magic 8-Ball's answer: " + answer)
